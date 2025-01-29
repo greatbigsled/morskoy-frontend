4335392367d88f15e8006c4e99ab87c8040ae04e
Alexander Ivanov <greatbigsled@gmail.com>
2025-01-30 01:44:16 +0300

    Add react-router, autoprefixer, postcss packages

---
+++ src/auth/Login.tsx
@@ -0,0 +1,20 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login page</h2>
+      <div>
+        <fieldset>
+          <input type="text"/>
+        </fieldset>
+        <fieldset>
+          <input type="password"/>
+        </fieldset>
+        <fieldset>
+          <button>Login</button>
+        </fieldset>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import {BrowserRouter, Route, Routes} from "react-router";
+
 import "./index.css";
 import App from "./App.tsx";
+import Login from "./auth/Login.tsx";
+import Register from "./auth/Register.tsx";
+import AuthLayout from "./auth/AuthLayout.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App/>}/>
+
+        <Route element={<AuthLayout/>}>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+,
 );
